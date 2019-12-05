c7182e857965785fdb7486ca82462d502c0a668a
youki <zyx7153841@126.com>
2019-12-05 10:17:44 +0000

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,24 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
-    // 短信验证码
-    Route::post('verificationCodes', 'VerificationCodesController@store')
-        ->name('verificationCodes.store');
-    // 用户注册
-    Route::post('users', 'UsersController@store')
-        ->name('users.store');
+Route::prefix('v1')
+    ->namespace('Api')
+    ->name('api.v1.')
+    ->group(function() {
+        // 使用throttle中间件限制访问频率
+        Route::middleware('throttle:' . config('api.rate_limits.sign'))
+            ->group(function(){
+                // 短信验证码
+                Route::post('verificationCodes', 'VerificationCodesController@store')
+                    ->name('verificationCodes.store');
+                // 用户注册
+                Route::post('users', 'UsersController@store')
+                    ->name('users.store');
+            });
+
+        Route::middleware('throttle:', config('api.rate_limits.access'))
+            ->group(function(){
+
+            });
 });
 
